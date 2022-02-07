d55759780fb83d892be8f35a0fda3771652d0846
Nicholas <nicholascomeau3@gmail.com>
2022-02-07 16:31:55 -0400

    Initial Project
    This is the initial project files with all the art assets in the project.

---
+++ Tania'sPresent/Assets/Scripts/MenuController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    //Menu object
+    //Blacksmith object
+
+    void Start()
+    {
+        //Get the menu object
+    }
+
+    void Update()
+    {
+        //if mouse cicked{ MouseClicked()}
+        
+    }
+
+    void MouseClicked()
+    {
+        //check if it hit a blacksmith{
+        //check if blacksmith is busy{
+        //if no then open the crafting menu and grab a reference to the gameobject}}
+    }
+
+    void CatagoryButtonPressed()//takes the item catagory as an argument
+    {
+        //open the catagory menu in the argument
+        //Check to activate new items availible to craft
+    }
+
+    void ItemButtonPress()//takes item you want to craft as argument
+    {
+        //Starts the crafting timer
+        //Makes the blacksmith busy
+        //Closes the menu system
+    }
+
+    void CloseAllMenu()
+    {
+        //close the menu system
+    }
+}
